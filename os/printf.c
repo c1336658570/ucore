5827662f8700d76b4068f60ae7ed10c37e614863
c1336658570 <1336658570@qq.com>
2023-05-13 20:57:29 +0800

    ch1笔记

--- os/printf.c
+++ os/printf.c
@@ -3,13 +3,14 @@
 #include "defs.h"
 static char digits[] = "0123456789abcdef";
 
+//打印xx，以base进制打印，sign是符号位
 static void printint(int xx, int base, int sign)
 {
 	char buf[16];
 	int i;
 	uint x;
 
-	if (sign && (sign = xx < 0))
+	if (sign && (sign = xx < 0))	//sign = 0时，不会执行后面那个语句，xx < 0时，sign=1，xx > 0时，sign=0
 		x = -xx;
 	else
 		x = xx;
@@ -26,13 +27,15 @@ static void printint(int xx, int base, int sign)
 		consputc(buf[i]);
 }
 
+//打印地址，以16进制方式
 static void printptr(uint64 x)
 {
 	int i;
 	consputc('0');
 	consputc('x');
-	for (i = 0; i < (sizeof(uint64) * 2); i++, x <<= 4)
-		consputc(digits[x >> (sizeof(uint64) * 8 - 4)]);
+	//循环输出x的16进制
+	for (i = 0; i < (sizeof(uint64) * 2); i++, x <<= 4)	//一个字节以16进制需要循环两次，所以循环上限为(sizeof(uint64) * 2)
+		consputc(digits[x >> (sizeof(uint64) * 8 - 4)]);  //每次提取最高4位，然后将其转成16进制，然后输出
 }
 
 // Print to the console. only understands %d, %x, %p, %s.
